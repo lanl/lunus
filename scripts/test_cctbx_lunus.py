01ce8ee085750e286fe0bd69ad07e6fa3a928ede
Mike Wall <mewall@lanl.gov>
2018-12-13 10:29:14 -0700

    add snase test. base version before refactoring lunus.c

--- scripts/test_cctbx_lunus.py
+++ scripts/test_cctbx_lunus.py
@@ -10,15 +10,9 @@ detector = img.get_detector()
 beam = img.get_beam()
 gonio = img.get_goniometer()
 scan = img.get_scan()
-data = img.get_raw_data()
-
-print data
-
-#print detector
-
-#print img
 
 A = LunusDIFFIMAGE()
+data = img.get_raw_data()
 A.set_image(data)
 A.LunusPunchim(1203,1250,2459,1314)
 A.LunusWindim(100,100,2362,2426)
@@ -26,5 +20,5 @@ A.LunusThrshim(0,50)
 A.LunusModeim(20)
 print "Mode filter finished."
 data2 = A.get_image();
-#dxtbx.format.FormatCBFMini.FormatCBFMini.as_file(detector,beam,gonio,scan,data,path,header_convention="GENERIC_MINI",det_type="GENERIC")
+dxtbx.format.FormatCBFMini.FormatCBFMini.as_file(detector,beam,gonio,scan,data,path,header_convention="GENERIC_MINI",det_type="GENERIC")
 FormatCBFMini.as_file(detector,beam,gonio,scan,data2,"g150aich_274_6_1_lunus_00001.cbf")
